dfda2d9ed3b2ce38ec2c70137095569b8e5d81d8
yrncollo <ngenocollins599@gmail.com>
2024-01-03 15:51:24 +0300

    test

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use colored::*;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
+fn main() {
+
+    let secret_number = rand::thread_rng().gen_range(1..=101);
+
+    println!("The secret_number is {secret_number}");
+
+   loop {
+       
+        println!("Guess the number");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+
+        };
+
+        println!("You have guessed: {guess}");
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("{}", "Almost there".yellow()),
+            Ordering::Greater => println!("{}", "Umeenda sana jameniii!!!! ".blue()),
+            Ordering::Equal => {
+                println!("{}", "You win".red().bold());
+                break;
+            }
+        }
+   } 
+}
