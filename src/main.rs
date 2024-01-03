2bfaf69b58fd6672fbf00e51af0b46e3f4be5c4d
yrncollo <ngenocollins599@gmail.com>
2024-01-03 16:24:20 +0300

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use rand::Rng;
 mod game_logic;
-
 fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=101);
 
-    println!("The secret_number is {secret_number}");
+    println!("The secret_number is {}",secret_number);
 
     game_logic::run(secret_number);
 
